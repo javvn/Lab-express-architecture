dd6ba524f07b0b57beba1192797e4311fc4840df
javvn <juho0428@naver.com>
2023-02-20 13:42:27 +0900

    [#1] create subnet

--- network/vpc.tf
+++ network/vpc.tf
@@ -13,4 +13,13 @@ module "vpc" {
   map_public_ip_on_launch = local.vpc_context.map_public_ip_on_launch
   create_igw              = local.vpc_context.create_igw
 
+  public_subnets  = local.subnet_groups_context["public"].cidr_block
+  private_subnets = local.subnet_groups_context["private"].cidr_block
+
+  vpc_tags                 = local.vpc_tags
+  igw_tags                 = local.igw_tags
+  public_subnet_tags       = local.public_subnet_tags
+  public_route_table_tags  = local.public_route_table_tags
+  private_subnet_tags      = local.private_subnet_tags
+  private_route_table_tags = local.private_route_table_tags
 }
