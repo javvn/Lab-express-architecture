e1fe5479ba6fee1c943747f448f709a9d9274270
javvn <juho0428@naver.com>
2023-02-21 16:25:16 +0900

    [#5] ec2 locals

--- ec2/_local.tf
+++ ec2/_local.tf
@@ -2,14 +2,14 @@ locals {
   common_tags             = data.terraform_remote_state.vpc.outputs.common_tags
   remote_context          = data.terraform_remote_state.vpc.outputs.context
   vpc_context             = data.terraform_remote_state.vpc.outputs.network.vpc
-  subnet_groups_context   = data.terraform_remote_state.vpc.outputs.network.vpc.subnet_groups
+  subnet_groups_context   = data.terraform_remote_state.vpc.outputs.network.subnet_groups
   security_groups_context = data.terraform_remote_state.vpc.outputs.network.security_groups
   context                 = yamldecode(file(var.config_file)).context
 
   ec2_context = { for k, v in yamldecode(templatefile(var.config_file, local.context)).ec2 : k => merge(v, {
-    name                   = "${local.remote_context.name}-ec2-${k}"
+    subnet_id              = local.subnet_groups_context[k]["${k}_subnets"][0]
+    user_data              = k == "private" ? file("${path.module}/scripts/userdata.sh") : ""
     vpc_security_group_ids = [for k, v in local.security_groups_context : v.security_group_id]
-    subnet_id              = local.subnet_groups_context[k].id[0]
     tags                   = merge(local.common_tags, { Name = "${local.remote_context.name}-ec2-${k}" })
   }) }
 
